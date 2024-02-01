c0feaf40c354c4a1647e297e1e75cdd375c99473
amogus-technologies <124402074+amogus-technologies@users.noreply.github.com>
2024-02-01 12:20:40 -0800

    removed unoptimized code

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 'use strict';
-module.exports = centerPad;
-
-function centerPad(str, len, ch) {
-  str = str.toString()
-  let padLength = len - str.length
-
-  ch ??= ' '
-
-  let splitPoint = Math.floor(str.length / 2)
-  let paddingData = ''
-  
-  while (paddingData.length < padLength) {
-    paddingData += ch
-  }
-
-  let paddedString = str.substring(0, splitPoint) + paddingData + str.substring(splitPoint)
-
-  return paddedString
-}
+module.exports = function centerPad(str, len, ch) {
+  let padLength = len - (str = toString.call(str)).length, splitPoint = Math.floor(str.length / 2), paddingData = '';
+  while (paddingData.length < padLength) paddingData += (ch.length > 1 ? ch.charCodeAt(0).toString(36).charAt(0) : ch) || ' ';
+  return str.slice(0, splitPoint) + paddingData + str.slice(splitPoint);
+};
 
