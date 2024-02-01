be3b41eb4a024967dd0ecb1e34792ad9127c93a7
amogus-technologies <124402074+amogus-technologies@users.noreply.github.com>
2024-02-01 12:22:59 -0800

    [object String]

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function centerPad(str, len, ch) {
-  let padLength = len - (str = toString.call(str)).length, splitPoint = Math.floor(str.length / 2), paddingData = '';
+  let padLength = len - (str = 'string' == typeof str ? str : toString.call(str)).length, splitPoint = Math.floor(str.length / 2), paddingData = '';
   while (paddingData.length < padLength) paddingData += (ch.length > 1 ? ch.charCodeAt(0).toString(36).charAt(0) : ch) || ' ';
   return str.slice(0, splitPoint) + paddingData + str.slice(splitPoint);
 };
